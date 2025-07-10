b7f1d3e5d4b0c814b5bf236edaad1eccb286e723
Jade Codes <jade_wilson66@hotmail.co.uk>
2025-07-10 22:22:34 +0000

    Optimise by reducing looping through way

--- graphhopper-web-demo/src/main/java/com/graphhopper/samples/parsers/AvgSpeedConditionalParser.java
+++ graphhopper-web-demo/src/main/java/com/graphhopper/samples/parsers/AvgSpeedConditionalParser.java
@@ -6,6 +6,9 @@ import com.graphhopper.routing.ev.EdgeIntAccess;
 import com.graphhopper.routing.util.parsers.TagParser;
 import com.graphhopper.storage.IntsRef;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.*;
 
 public class AvgSpeedConditionalParser implements TagParser {
@@ -30,22 +33,33 @@ public class AvgSpeedConditionalParser implements TagParser {
         if (tag == null)
             return;
 
-        Pattern entryPattern = Pattern.compile("([\\d.]+)\\s*@\\s*\\(([^)]+)\\)");
-        Matcher matcher = entryPattern.matcher(tag);
+        String currentCondition = conditionMap.get(encodedValueKey);
+        Pattern pattern = Pattern.compile("([\\d.]+)\\s*@\\s*\\((" + Pattern.quote(currentCondition) + ")\\)");
+        Matcher matcher = pattern.matcher(tag);
 
-        while (matcher.find()) {
+        if (matcher.find()) {
             double speed = Double.parseDouble(matcher.group(1));
-            String condition = matcher.group(2).toLowerCase().replaceAll("[^a-z0-9:_]", "_").replace(":", "");
-
-            String conditionKey = reverse ? "avgspeed_" + condition + "_backward"
-                    : "avgspeed_" + condition + "_forward";
-
-            if (conditionKey.equals(encodedValueKey + "_forward") && !reverse) {
-                avgSpeedEnc.setDecimal(false, edgeId, edgeIntAccess, speed);
-            } else if (conditionKey.equals(encodedValueKey + "_backward") && reverse) {
-                avgSpeedEnc.setDecimal(true, edgeId, edgeIntAccess, speed);
-            }
+            avgSpeedEnc.setDecimal(reverse, edgeId, edgeIntAccess, speed);
         }
         return;
     }
+    
+    private final HashMap<String, String> conditionMap = new HashMap<>() {
+        {
+            put("avgspeed_mo_fr_0400_0700", "Mo-Fr 04:00-07:00");
+            put("avgspeed_mo_fr_0700_0900", "Mo-Fr 07:00-09:00");
+            put("avgspeed_mo_fr_0900_1200", "Mo-Fr 09:00-12:00");
+            put("avgspeed_mo_fr_1200_1400", "Mo-Fr 12:00-14:00");
+            put("avgspeed_mo_fr_1400_1600", "Mo-Fr 14:00-16:00");
+            put("avgspeed_mo_fr_1600_1900", "Mo-Fr 16:00-19:00");
+            put("avgspeed_mo_fr_1900_2200", "Mo-Fr 19:00-22:00");
+            put("avgspeed_mo_fr_2200_0400", "Mo-Fr 22:00-04:00");
+            put("avgspeed_sa_su_0400_0700", "Sa-Su 04:00-07:00");
+            put("avgspeed_sa_su_0700_1000", "Sa-Su 07:00-10:00");
+            put("avgspeed_sa_su_1000_1400", "Sa-Su 10:00-14:00");
+            put("avgspeed_sa_su_1400_1900", "Sa-Su 14:00-19:00");
+            put("avgspeed_sa_su_1900_2200", "Sa-Su 19:00-22:00");
+            put("avgspeed_sa_su_2200_0400", "Sa-Su 22:00-04:00");
+        }
+    };
 }
